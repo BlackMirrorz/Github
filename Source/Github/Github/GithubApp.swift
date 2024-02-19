c89c240873b3fe402ec2980d763dbb5c1a6d3b68
PongoBongoSaurus <35516944+BlackMirrorz@users.noreply.github.com>
2024-02-19 11:20:22 +0900

    Initial Commit

---
+++ Source/Github/Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by Josh Robbins on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
