cbe6316b0013618c992de4ce8ba707be4bf2623a
PongoBongoSaurus <35516944+BlackMirrorz@users.noreply.github.com>
2024-02-22 17:04:04 +0900

    Test

--- Source/Github/ContentView.swift
+++ Source/Github/ContentView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+    
+  
+  
+  
+  
+  
+  var body: some View {
         VStack {
-            Image(systemName: "globe")
+                      Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world!asasasa")
         }
         .padding()
     }
