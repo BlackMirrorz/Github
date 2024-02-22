cac2eebc9b4ad8053e62d1785b64fb1d639f3139
PongoBongoSaurus <35516944+BlackMirrorz@users.noreply.github.com>
2024-02-22 17:11:40 +0900

    assaas

--- Source/Github/ContentView.swift
+++ Source/Github/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
                       Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!asasasaasassasasasassasdsdsdsdsdsdsdsdasasassasasa")
+            Text("Hello, world!asasasaasassasasasassdsssdsdsdsdsddssdsdsddssddsdsdsdsdsdssddsdssddsasdsdsdsdsdsdsdsdasasassasasa")
         }
         .padding()
     }
